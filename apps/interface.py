a6c8d81a1120c961f1e41bace4e87bc1eade73fd
michael ramboer <michaelramboer123@gmail.com>
2025-08-29 12:04:32 +0200

    inter

--- apps/interface.py
+++ apps/interface.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write('Welcome to boost_medium app!')
+st.markdown("""
+    # Boost Medium Articles App
+""")
+
+# CSS = """
+# .stApp {
+#     background-image: url(https://raw.githubusercontent.com/nhayari/boost_medium/refs/heads/streamlit/apps/medium.webp) !important;
+#     background-position: top center;
+#     background-repeat: no-repeat;
+#     background-size: 300px auto;
+# }
+# """
+# st.markdown(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+st.button("Evaluate Model")
